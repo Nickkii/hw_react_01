88865ea545154cff4b58de0ace753dc6fb6004ea
Veronika Dubyk <veronicadubik@gmail.com>
2021-11-25 22:30:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   
+    let books = [
+      {id: 1, autor: 'Лев Тол­стой	', name: 'Вой­на и мир	', year: '1869'},
+      {id: 2, autor: 'Джордж Оруэлл		', name: '1984', year: '1949'},
+      {id: 3, autor: 'Джеймс Джойс		', name: 'Улисс', year: '1922'},
+      {id: 4, autor: 'Вла­ди­мир Набо­ков		', name: 'Лолита', year: '1955'},
+      {id: 5, autor: 'Уильям Фолк­нер		', name: 'Шум и ярость	', year: '1929'},
+   ];
+
+   let res = books.map(function(item) {
+      return <tr key={item.id}>
+         <td>{item.autor}</td>
+         <td>{item.name}</td>
+         <td>{item.year}</td>
+      </tr>;
+   });
+   
+   return  <table>
+      <thead>
+         <tr>
+            <td>Автор</td>
+            <td>Название книги</td>
+            <td>Год выпуска</td>
+         </tr>
+      </thead>
+      <tbody>
+         {res}
+      </tbody>
+   </table>;
+  }
+
+
+
 
 export default App;
